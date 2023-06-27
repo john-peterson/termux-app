3247f0e332305ff695685e7405ffb7220e3ef7b5
John Sebastian Peterson <jpeterson57@gmail.com>
2023-06-27 07:48:40 +0000

    Adjust keyboard height when changing keyboard

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -531,7 +531,7 @@ public final class TermuxActivity extends AppCompatActivity implements ServiceCo
         terminalToolbarViewPager.addOnPageChangeListener(new TerminalToolbarViewPager.OnPageChangeListener(this, terminalToolbarViewPager));
     }
 
-    private void setTerminalToolbarHeight() {
+    public void setTerminalToolbarHeight() {
         final ViewPager terminalToolbarViewPager = getTerminalToolbarViewPager();
         if (terminalToolbarViewPager == null) return;
 
